ae03b872dbb8a94fdd8809954f443e64ba10e590
tawseef <tawseefalam1@gmail.com>
2024-09-25 20:34:56 +0530

    Navbar Created

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Navbar.style.css";
+
+function Navbar() {
+  return (
+    <div className='navbarContainer'>
+        Expense Tracker
+    </div>
+  )
+}
+
+export default Navbar
